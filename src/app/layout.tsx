aaefd2578ce750c13874774a92f589073f00577e
yunusmujadidi <yunusmujadidi@gmail.com>
2025-06-23 00:11:37 +0700

    fix: next js hydration mismatch error xd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
+        suppressHydrationWarning={true}
       >
         <SidebarProvider>
           <AppSidebar />
